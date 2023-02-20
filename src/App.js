29e8283a0537bf8f80382c3c5d870fcb5a42a123
Chadly Riedewald <no925.c@gmail.com>
2023-02-20 18:13:19 +0100

    Created typography components (Heading & Text)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import { globalStyles } from 'stitches.config';
+import { Heading, Text } from 'components/typography';
 
 const App = () => {
   globalStyles();
 
-  return <>Hello World</>;
+  return (
+    <>
+      <Heading variant="h1" weight="semibold">
+        Hello World!
+      </Heading>
+      <Heading variant="h2" weight="semibold">
+        Hello World!
+      </Heading>
+      <Heading variant="h3" weight="semibold">
+        Hello World!
+      </Heading>
+      <Heading variant="h4" weight="semibold">
+        Hello World!
+      </Heading>
+      <Heading variant="h5" weight="semibold">
+        Hello World!
+      </Heading>
+      <Heading variant="h6" weight="semibold">
+        Hello World!
+      </Heading>
+      <Text size="xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro!
+      </Text>
+      <Text size="lg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro!
+      </Text>
+      <Text size="md">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro!
+      </Text>
+      <Text size="sm">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro!
+      </Text>
+      <Text size="xs">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro!
+      </Text>
+    </>
+  );
 };
 
 export default App;
